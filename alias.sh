f88e3df476e5a48479c13a129a5a7e210181af6c
Nicolaj Figaw <figaw@hotmail.com>
2020-01-13 19:49:00 +0100

    FIX alias, now.json

--- alias.sh
+++ alias.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-alias docker-node='docker run -it -v "$(pwd)":/node -w /node node:13.6.0-alpine3.11'
+alias docker-node='docker run --rm -it -v "$(pwd)":/node -w /node node:13.6.0-alpine3.11'
 alias npx='docker-node npx'
 alias node='docker-node node'
+alias npm='docker-node npm'
 
-alias yarn='docker run -it \
+alias yarn='docker run --rm -it \
     -v "$(pwd)":/node \
     -w /node \
     -p 3000:3000 \
